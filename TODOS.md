68c60b9d46a13fe9bf357f3108375a3aaa658861
monsieurlazar <dasidasi86@gmail.com>
2025-06-11 19:43:11 +0300

    todos

---
+++ TODOS.md
@@ -0,0 +1,8 @@
+- ask for pgin id like com.bylazar.myplugin
+
+
+telnet localhost 5554
+auth TOKEN
+
+redir add tcp:8001:8001
+redir add tcp:8002:8002
--- cli/template/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configs/Configs.kt
+++ cli/template/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configs/Configs.kt
@@ -1,13 +1,6 @@
 package org.firstinspires.ftc.teamcode.configs
 
-import com.bylazar.limelightproxy.ProxyPluginConfig
 import com.bylazar.mylibrary.MyConfig
-
-
-class ProxyConfig : ProxyPluginConfig(){
-    override var isDev = true
-    override var customString = "HIII!"
-}
 
 class MyConfig : MyConfig(){
     override var isDev = true
