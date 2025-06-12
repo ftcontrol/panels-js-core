ef0eb027d48e5b0d31c70595cc21659bf1fa5452
monsieurlazar <dasidasi86@gmail.com>
2025-06-12 18:43:40 +0300

    fixed some stuff

--- new-app/dist/counter.js
+++ new-app/dist/counter.js
@@ -9,14 +9,35 @@ if (typeof window !== 'undefined') {
 	((window.__svelte ??= {}).v ??= new Set()).add(PUBLIC_VERSION);
 }
 
-var root = $.from_html(`<p></p>`);
-
-function Counter($$anchor) {
-	var p = root();
-
-	p.textContent = '0';
-	$.append($$anchor, p);
+function increment(n) {
+    return n + 1;
 }
+
+var on_click = (_, count) => {
+	$.set(count, increment($.get(count)), true);
+};
+
+var root = $.from_html(`<p class="svelte-17bjde4"> </p> <button>Increment</button>`, 1);
+
+function Counter($$anchor, $$props) {
+	$.push($$props, true);
+
+	let count = $.state(0);
+	var fragment = root();
+	var p = $.first_child(fragment);
+	var text = $.child(p, true);
+
+	$.reset(p);
+
+	var button = $.sibling(p, 2);
+
+	button.__click = [on_click, count];
+	$.template_effect(() => $.set_text(text, $.get(count)));
+	$.append($$anchor, fragment);
+	$.pop();
+}
+
+$.delegate(['click']);
 
 export { Counter as default };
 //# sourceMappingURL=counter.js.map
--- new-app/src/lib/Counter.svelte
+++ new-app/src/lib/Counter.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+  import { increment } from "./index"
   let count: number = $state(0)
 </script>
 
 <p>{count}</p>
+<button
+  onclick={() => {
+    count = increment(count)
+  }}>Increment</button
+>
+
+<style>
+  p {
+    color: red;
+  }
+</style>
