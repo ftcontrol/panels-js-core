62de032f295143aa30d45658daab86daf042a351
lazar <dasidasi86@gmail.com>
2025-07-16 01:28:12 +0300

    toggle animation

--- cli/core/ui/Toggle.svelte
+++ cli/core/ui/Toggle.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  import { onMount } from "svelte"
+  import { tick, onMount } from "svelte"
+
   type Snippet<Props = any> = (props: Props) => any
+
   let {
     trigger,
     content,
@@ -10,16 +12,43 @@
   } = $props()
 
   let isOpen = $state(false)
+  let shouldRender = $state(false)
+  let container: HTMLDivElement | null = null
 
-  function toggle(event?: MouseEvent) {
-    if (event) {
-      event.preventDefault()
+  function toggle(event?: MouseEvent | KeyboardEvent) {
+    event?.preventDefault()
+    if (isOpen) {
+      close()
+    } else {
+      open()
     }
-    isOpen = !isOpen
   }
 
   function close() {
+    if (container) {
+      container.style.height = container.scrollHeight + "px"
+      container.offsetHeight
+      container.style.height = "0"
+    }
     isOpen = false
+  }
+
+  function open() {
+    shouldRender = true
+    tick().then(() => {
+      if (container) {
+        container.style.height = container.scrollHeight + "px"
+      }
+    })
+    isOpen = true
+  }
+
+  function onTransitionEnd() {
+    if (!isOpen) {
+      shouldRender = false
+    } else if (container) {
+      container.style.height = "auto"
+    }
   }
 </script>
 
@@ -29,11 +58,9 @@
   role="button"
   tabindex="0"
   onmousedown={(event: MouseEvent) => {
-    if (event.detail > 1) {
-      event.preventDefault()
-    }
+    if (event.detail > 1) event.preventDefault()
   }}
-  onkeydown={(e) => {
+  onkeydown={(e: KeyboardEvent) => {
     if (e.key === "Enter" || e.key === " ") {
       e.preventDefault()
       toggle()
@@ -43,13 +70,33 @@
   {@render trigger({ isOpen })}
 </div>
 
-{#if isOpen}
-  {@render content({ close })}
+{#if shouldRender}
+  <div
+    bind:this={container}
+    class="content-container"
+    data-open={isOpen}
+    ontransitionend={onTransitionEnd}
+    style="height: 0; overflow: hidden"
+  >
+    {@render content({ close })}
+  </div>
 {/if}
 
 <style>
   .trigger {
     display: block;
     cursor: pointer;
+    user-select: none;
+  }
+
+  .content-container {
+    transition:
+      height 250ms ease,
+      opacity 250ms ease;
+    opacity: 0;
+  }
+
+  .content-container[data-open="true"] {
+    opacity: 1;
   }
 </style>
