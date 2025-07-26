13a04250e070ac72ea48c17d751426843bf358d3
lazar <dasidasi86@gmail.com>
2025-07-27 01:21:35 +0300

    ui lib for docs

---
+++ cli/core/ui/docs/Title.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Snippet } from "svelte"
+
+  let { children }: { children?: Snippet } = $props()
+</script>
+
+<h1>{@render children?.()}</h1>
---
+++ cli/docs.ts
@@ -0,0 +1,4 @@
+import Title from "./core/ui/docs/Title.svelte"
+import Paragraph from "./core/ui/docs/Paragraph.svelte"
+
+export { Title, Paragraph }
