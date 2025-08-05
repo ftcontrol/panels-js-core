4fb5f5f28185bd140da977c2a9cbd09ab0fe145c
lazar <dasidasi86@gmail.com>
2025-08-06 01:13:33 +0300

    images and videos

--- cli/docs.ts
+++ cli/docs.ts
@@ -3,5 +3,7 @@ import Paragraph from "./core/ui/docs/Paragraph.svelte"
 import Accent from "./core/ui/docs/Accent.svelte"
 import Link from "./core/ui/docs/Link.svelte"
 import ListItem from "./core/ui/docs/ListItem.svelte"
+import Image from "./core/ui/docs/Image.svelte"
+import Video from "./core/ui/docs/Video.svelte"
 
-export { Title, Paragraph, Accent, Link, ListItem }
+export { Title, Paragraph, Accent, Link, ListItem, Image, Video }
