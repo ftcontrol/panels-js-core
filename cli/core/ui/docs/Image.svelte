f09e9d41b4a2640dc117e7d4260bc0b2cd97508e
lazar <dasidasi86@gmail.com>
2025-08-06 01:13:36 +0300

    Create Image.svelte

---
+++ cli/core/ui/docs/Image.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let {
+    src,
+    alt = "Image",
+  }: {
+    src: string
+    alt?: string
+  } = $props()
+</script>
+
+<img {src} {alt} />
+<p>{alt}</p>
+
+<style>
+  img,
+  p {
+    width: 100%;
+    max-width: 500px;
+  }
+  p {
+    text-align: center;
+    opacity: 0.5;
+  }
+  img {
+    border: 1px solid currentColor;
+  }
+</style>
