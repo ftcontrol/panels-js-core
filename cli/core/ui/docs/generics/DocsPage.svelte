ce57cd2b5097eaf5c80f213d3304eb29d7c58a5c
lazar <dasidasi86@gmail.com>
2025-08-21 15:23:08 +0300

    removed panels from docs page

--- cli/core/ui/docs/generics/DocsPage.svelte
+++ cli/core/ui/docs/generics/DocsPage.svelte
@@ -1,184 +1,201 @@
 <script lang="ts">
-    import {type Snippet} from "svelte"
-    import Toggle from "../../Toggle.svelte";
-    import Arrow from "../../Arrow.svelte";
-    import {type PluginConfig} from "../../../types"
-    import HeadingsOverlay from "./HeadingsOverlay.svelte";
-    import Button from "../../Button.svelte";
+  import { type Snippet } from "svelte"
+  import Toggle from "../../Toggle.svelte"
+  import Arrow from "../../Arrow.svelte"
+  import { type PluginConfig } from "../../../types"
+  import HeadingsOverlay from "./HeadingsOverlay.svelte"
+  import Button from "../../Button.svelte"
 
-    let {plugins, skippedPlugins = [], children}: {
-        plugins: PluginConfig[];
-        skippedPlugins?: PluginConfig[];
-        children?: Snippet
-    } = $props()
+  let {
+    plugins,
+    skippedPlugins = [],
+    children,
+  }: {
+    plugins: PluginConfig[]
+    skippedPlugins?: PluginConfig[]
+    children?: Snippet
+  } = $props()
 
-    let orderedPlugins = $derived.by(() => {
-        const docsPlugin = plugins.find(
-            (it) => it.id === "com.bylazar.docs"
-        )
-        const otherPlugins = plugins.filter(
-            (it) => it.id !== "com.bylazar.docs"
-        )
+  let orderedPlugins = $derived.by(() => {
+    const p = plugins.filter((it) => it.id != "com.bylazar.panels")
+    const docsPlugin = p.find((it) => it.id === "com.bylazar.docs")
+    const otherPlugins = p.filter((it) => it.id !== "com.bylazar.docs")
 
-        if (!docsPlugin) {
-            return otherPlugins
-        }
+    if (!docsPlugin) {
+      return otherPlugins
+    }
 
-        const renamedDocsPlugin = {
-            ...docsPlugin,
-            name: "Core"
-        }
+    const renamedDocsPlugin = {
+      ...docsPlugin,
+      name: "Core",
+    }
 
-        return [renamedDocsPlugin, ...otherPlugins.sort((a, b) => a.name.localeCompare(b.name))]
-    })
+    return [
+      renamedDocsPlugin,
+      ...otherPlugins.sort((a, b) => a.name.localeCompare(b.name)),
+    ]
+  })
 
-    let isOpened = $state(false)
-
-
-
+  let isOpened = $state(false)
 </script>
 
-<button class="bg" class:isOpened aria-label="Close Navbar" onclick={()=>{isOpened = false}}></button>
+<button
+  class="bg"
+  class:isOpened
+  aria-label="Close Navbar"
+  onclick={() => {
+    isOpened = false
+  }}
+></button>
 
 <section>
-    <div class="button nav" class:isOpened>
-        <Button onclick={()=>{isOpened = !isOpened}}>Content</Button>
-    </div>
-    <nav class:isOpened>
-        {#each orderedPlugins as plugin}
-            <Toggle defaultOpen={plugin.id === "com.bylazar.docs"}>
-                {#snippet trigger({isOpen})}
-                    <p>
-                        {plugin.name}
-                        <Arrow {isOpen}/>
-                    </p>
-                {/snippet}
-                {#snippet content({close})}
-                    <div class="item">
-                        {#each (plugin.components || []).filter(it => it.type === "docs") as c}
-                            <a onclick={()=>{isOpened = false}} href="/docs/{plugin.id}/{c.id}">{c.id}</a>
-                        {/each}
-                    </div>
-                {/snippet}
-            </Toggle>
-            <div class="divider"></div>
-        {/each}
-        {#each skippedPlugins as plugin}
-            <p class="disabled">{plugin.name}</p>
-        {/each}
-    </nav>
-    <div class="content">
-        <HeadingsOverlay/>
-        {@render children?.()}
-    </div>
+  <div class="button nav" class:isOpened>
+    <Button
+      onclick={() => {
+        isOpened = !isOpened
+      }}>Content</Button
+    >
+  </div>
+  <nav class:isOpened>
+    {#each orderedPlugins as plugin}
+      <Toggle defaultOpen={plugin.id === "com.bylazar.docs"}>
+        {#snippet trigger({ isOpen })}
+          <p>
+            {plugin.name}
+            <Arrow {isOpen} />
+          </p>
+        {/snippet}
+        {#snippet content({ close })}
+          <div class="item">
+            {#each (plugin.components || []).filter((it) => it.type === "docs") as c}
+              <a
+                onclick={() => {
+                  isOpened = false
+                }}
+                href="/docs/{plugin.id}/{c.id}">{c.id}</a
+              >
+            {/each}
+          </div>
+        {/snippet}
+      </Toggle>
+      <div class="divider"></div>
+    {/each}
+    {#each skippedPlugins as plugin}
+      <p class="disabled">{plugin.name}</p>
+    {/each}
+  </nav>
+  <div class="content">
+    <HeadingsOverlay />
+    {@render children?.()}
+  </div>
 </section>
 
 <style>
-    section {
-        display: flex;
-        gap: calc(var(--padding) / 2);
-        margin: 0.5rem;
-        overflow-y: auto;
-        height: 100%;
-    }
+  section {
+    display: flex;
+    gap: calc(var(--padding) / 2);
+    margin: 0.5rem;
+    overflow-y: auto;
+    height: 100%;
+  }
 
-    p {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        gap: var(--padding);
-        font-weight: 700;
-    }
+  p {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: var(--padding);
+    font-weight: 700;
+  }
 
-    a {
-        color: inherit;
-        text-decoration: none;
-        display: block;
-        margin-bottom: 0.25rem;
-    }
+  a {
+    color: inherit;
+    text-decoration: none;
+    display: block;
+    margin-bottom: 0.25rem;
+  }
 
+  nav {
+    padding: var(--padding);
+    overflow-y: auto;
+    background-color: var(--bgMedium);
+    border-radius: 1rem;
+    min-width: 200px;
+  }
+
+  .content {
+    background-color: var(--bgMedium);
+    border-radius: 1rem;
+    padding: var(--padding);
+    width: 100%;
+    overflow-y: auto;
+  }
+
+  p.disabled {
+    opacity: 0.5;
+  }
+
+  .divider {
+    width: 100%;
+    height: 1px;
+    background-color: currentColor;
+    margin-block: 1rem;
+    opacity: 0.5;
+  }
+
+  .button.nav {
+    display: none;
+    position: fixed;
+    z-index: 100;
+    right: calc(var(--padding) / 1);
+    bottom: calc(var(--padding) / 1);
+  }
+
+  button.bg {
+    all: unset;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: var(--bgDark);
+    opacity: 0;
+    cursor: pointer;
+    z-index: 99;
+    transition: opacity 0.5s;
+    pointer-events: none;
+  }
+
+  @media only screen and (max-width: 900px) {
     nav {
-        padding: var(--padding);
-        overflow-y: auto;
-        background-color: var(--bgMedium);
-        border-radius: 1rem;
-        min-width: 200px;
+      position: fixed;
+      top: calc(var(--padding) / 2);
+      left: calc(var(--padding) / 2);
+      bottom: calc(var(--padding) / 2);
+      transform: translateX(-120%);
+      transition: transform 0.5s;
+      height: calc(100% - var(--padding));
+      z-index: 100;
     }
 
-    .content {
-        background-color: var(--bgMedium);
-        border-radius: 1rem;
-        padding: var(--padding);
-        width: 100%;
-        overflow-y: auto;
-    }
-
-    p.disabled {
-        opacity: 0.5;
-    }
-
-    .divider {
-        width: 100%;
-        height: 1px;
-        background-color: currentColor;
-        margin-block: 1rem;
-        opacity: 0.5;
+    nav.isOpened {
+      transform: translateX(0);
     }
 
     .button.nav {
-        display: none;
-        position: fixed;
-        z-index: 100;
-        right: calc(var(--padding) / 1);
-        bottom: calc(var(--padding) / 1);
+      display: block;
+      transform: translateX(0);
+      transition: transform 0.5s;
+    }
+    .button.nav.isOpened {
+      transform: translateX(150%);
     }
 
     button.bg {
-        all: unset;
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: var(--bgDark);
-        opacity: 0;
-        cursor: pointer;
-        z-index: 99;
-        transition: opacity 0.5s;
-        pointer-events: none;
+      display: block;
     }
-
-    @media only screen and (max-width: 900px) {
-        nav {
-            position: fixed;
-            top: calc(var(--padding) / 2);
-            left: calc(var(--padding) / 2);
-            bottom: calc(var(--padding) / 2);
-            transform: translateX(-120%);
-            transition: transform 0.5s;
-            height: calc(100% - var(--padding));
-            z-index: 100;
-        }
-
-        nav.isOpened {
-            transform: translateX(0);
-        }
-
-        .button.nav {
-            display: block;
-            transform: translateX(0);
-            transition: transform 0.5s;
-        }
-        .button.nav.isOpened {
-            transform: translateX(150%);
-        }
-
-        button.bg {
-            display: block;
-        }
-        button.bg.isOpened{
-            opacity: 0.5;
-            pointer-events: all;
-        }
+    button.bg.isOpened {
+      opacity: 0.5;
+      pointer-events: all;
     }
+  }
 </style>
